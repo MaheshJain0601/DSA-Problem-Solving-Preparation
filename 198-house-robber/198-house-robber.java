69d82cf836f02cf521bcd7321062bea7d97ec57d
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2022-10-01 11:15:29 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (61.85%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+class Solution {
+    // 1. Recursive solution
+    private int recursiveRob(int[] nums, int index) {
+        if (index == 0) {
+            return nums[index];
+        }
+        if (index < 0) {
+            return 0;
+        }
+        
+        int robbed = nums[index] + recursiveRob(nums, index - 2);
+        int notRobbed = 0 + recursiveRob(nums, index - 1);
+        return Math.max(robbed, notRobbed);
+    }
+    
+    // 2. Memoization Rob using DP
+    private int recursiveRobMemoize(int[] nums, int index, int[] dp) {
+        if (index == 0) {
+            return nums[index];
+        }
+        if (index < 0) {
+            return 0;
+        }
+        
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        
+        int robbed = nums[index] + recursiveRob(nums, index - 2);
+        int notRobbed = 0 + recursiveRob(nums, index - 1);
+        dp[index] = Math.max(robbed, notRobbed);
+        return dp[index];
+    }
+    
+    // 3. Iterative Tabulation in DP
+    private int iterativeTabRob(int[] nums, int length) {
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        
+        
+        for (int index = 1; index < length; index++) {
+            int take = nums[index];
+            if (index > 1) {
+                take += dp[index-2];
+            }
+            int notTake = 0 + dp[index-1];
+            dp[index] = Math.max(take, notTake);
+        }
+        return dp[length - 1];
+        
+    }
+    
+    public int rob(int[] nums) {
+        // 1. Recursive Rob
+        // return recursiveRob(nums, nums.length - 1);
+        
+        // 2. Memoization Rob using DP
+        //int[] dp = new int[nums.length + 1];
+        //Arrays.fill(dp, -1);
+        //return recursiveRobMemoize(nums, nums.length - 1, dp);
+        
+        // 3. Iterative Tabulation in DP
+        return iterativeTabRob(nums, nums.length);
+    }
+}
