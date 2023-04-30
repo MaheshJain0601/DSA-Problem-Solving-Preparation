dc6cc9473c5c82134efed8ee3dffe95b048ec657
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2023-04-30 15:12:56 +0530

    Time: 1 ms (100.00%), Space: 45.5 MB (5.11%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sumEvenValuedGrandParents = 0;
+    
+    public void dfsTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.val % 2 == 0) {
+            if (root.left != null) {
+                if (root.left.left != null) {
+                    sumEvenValuedGrandParents += root.left.left.val;
+                }
+                if (root.left.right != null) {
+                    sumEvenValuedGrandParents += root.left.right.val;
+                }
+            }
+            if (root.right != null) {
+                if (root.right.left != null) {
+                    sumEvenValuedGrandParents += root.right.left.val;
+                }
+                if (root.right.right != null) {
+                    sumEvenValuedGrandParents += root.right.right.val;
+                }
+            }
+        }
+        dfsTraversal(root.left);
+        dfsTraversal(root.right);
+    }
+    public int sumEvenGrandparent(TreeNode root) {
+        dfsTraversal(root);
+        return sumEvenValuedGrandParents;
+    }
+}
