0c6829400ebfe9889578a4b8c91170db4b7127ee
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2023-10-06 15:41:13 +0530

    Time: 1 ms (90.75%), Space: 41.2 MB (52.15%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new LinkedList<>();
@@ -21,27 +22,34 @@ class Solution {
         }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        boolean flag = true;
-        while(!queue.isEmpty()) {
-            int num = queue.size();
-            LinkedList<Integer> level = new LinkedList<>();
-            for (int i = 0; i < num; i++) {
-                TreeNode temp = queue.poll();
-                if (flag) {
-                    level.add(temp.val);
+        
+        int size;
+        boolean leftToRight = true;
+        
+        while (!queue.isEmpty()) {
+            size = queue.size();
+            LinkedList<Integer> subList = new LinkedList<>();
+            
+            for (int index = 0; index < size; index++) {
+                TreeNode node = queue.poll();
+                if(leftToRight) {
+                    subList.add(node.val);
                 } else {
-                    level.addFirst(temp.val);
+                    subList.addFirst(node.val);
                 }
-                if (temp.left != null) {
-                    queue.offer(temp.left);
-                } 
-                if (temp.right != null) {
-                    queue.offer(temp.right);
+                
+                if (node.left != null) {
+                    queue.offer(node.left);
                 }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                
             }
-            result.add(level);
-            flag = ! flag;
+            result.add(subList);
+            leftToRight = !leftToRight;
         }
+        
         return result;
     }
 }
