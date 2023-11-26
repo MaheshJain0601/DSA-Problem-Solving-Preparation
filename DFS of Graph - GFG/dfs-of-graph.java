ba524a02cfe799a688510e66243277b2c36e83e8
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2023-11-26 13:37:40 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    private void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, ArrayList<Integer> dfsResult) {
+        visited[node] = true;
+        dfsResult.add(node);
+        
+        for (Integer adjListElement: adj.get(node)) {
+            if (visited[adjListElement] == false) {
+                dfs(adjListElement, adj, visited, dfsResult);
+            }
+        }
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> dfsResult = new ArrayList<>();
+        
+        boolean[] visited = new boolean[V];
+        
+        Arrays.fill(visited, false);
+        
+        dfs(0, adj, visited, dfsResult);
+        
+        return dfsResult;
+    }
+}
