741c1df1c1bf19751ff78582efbc81a6543dab9d
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2023-12-04 15:20:28 +0530

    Time: 17 ms (75.11%), Space: 55.5 MB (74.61%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,15 +1,38 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        // Approach - 1
+//         Map<Integer, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < nums.length; ++i) {
-            if (map.containsKey(nums[i])) {
-                Integer value = map.get(nums[i]);
-                if (Math.abs(value.intValue() - i) <= k) {
-                    return true;
-                }
+//         for (int i = 0; i < nums.length; ++i) {
+//             if (map.containsKey(nums[i])) {
+//                 Integer value = map.get(nums[i]);
+//                 if (Math.abs(value.intValue() - i) <= k) {
+//                     return true;
+//                 }
+//             }
+//             map.put(nums[i], i);
+//         }
+        
+//         return false;
+        
+        // if i and j are both of same indices
+        if (k == 0) {
+            return false;
+        }
+        
+        Set<Integer> slidingWindowHash = new HashSet<>();
+        
+        for (int index = 0; index < nums.length; ++index) {
+            
+            if (slidingWindowHash.contains(nums[index])) {
+                return true;
             }
-            map.put(nums[i], i);
+            
+            if (index >= k) {
+                slidingWindowHash.remove(nums[index-k]);
+            }
+            
+            slidingWindowHash.add(nums[index]);
         }
         
         return false;
