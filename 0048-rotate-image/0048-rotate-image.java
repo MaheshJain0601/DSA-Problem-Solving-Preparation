cf394125014c04f517a7bdb99ca5f124b189fcb2
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2024-12-04 09:48:34 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (31.67%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+     private void swap(int[] nums, int start, int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+    public void rotate(int[][] matrix) {
+        for (int row = 0; row < matrix.length; ++row) {
+            for (int col = row; col < matrix[row].length; ++col) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }
+        for (int row = 0; row < matrix.length; ++row) {
+            reverse(matrix[row], 0, matrix[row].length-1);
+        }
+    }
+}
