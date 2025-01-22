5ed43b4580935efa1e72f9322fa50a31b9487f19
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-01-22 15:32:16 +0530

    Time: 4 ms (46.44%), Space: 45.4 MB (78.01%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,11 +1,46 @@
 class Solution {
+
+    /**
+        Approach-2: Better
+        Maintaining state of the index; instead re-iterating again and again
+        TC: O(N^2)
+        SC: O(2*N)
+     */
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+
+        int N = A.length;
+
+        int[] result = new int[N];
+
+        boolean[] isPresentA = new boolean[N+1];
+        boolean[] isPresentB = new boolean[N+1];
+
+        int count;
+
+        for (int index = 0; index < N; ++index) {
+            
+            isPresentA[A[index]] = true;
+            isPresentB[B[index]] = true;
+
+            count = 0;
+            for (int i = 1; i <= N; ++i) {
+                if (isPresentA[i] && isPresentB[i]) {
+                    count++;
+                }
+            }
+            result[index] = count;
+        }
+        
+        return result;
+    }
+
     /**
         Approach - 1: Brute force
         Compare each and every element of A and B and populate C
         TC: O(N^3)
         SC: O(1)
      */
-    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+    public int[] findThePrefixCommonArrayApproach1(int[] A, int[] B) {
 
         int N = A.length;
 
