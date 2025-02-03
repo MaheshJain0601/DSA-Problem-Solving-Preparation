ac50b567376a8e01bd233f2aaccd17780a67eb2e
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-02-03 10:25:48 +0530

    Time: 0 ms (100%), Space: 43.2 MB (63.95%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,8 +1,20 @@
 class Solution {
-    // Approch 1:
+    // Approch 2:
     // TC: O(N)
     // SC: O(1)
     public boolean isArraySpecial(int[] nums) {
+        for (int index = 0; index < nums.length - 1; ++index) {
+            if ((nums[index] & 1) == (nums[index+1] & 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Approch 1:
+    // TC: O(N)
+    // SC: O(1)
+    public boolean isArraySpecialApproach1(int[] nums) {
         for (int index = 0; index < nums.length - 1; ++index) {
             if (nums[index]%2 == nums[index+1]%2) {
                 return false;
