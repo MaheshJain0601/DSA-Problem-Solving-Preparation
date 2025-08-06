abd913e068560aaeb4a3ccc7314f0b45a6c5d12c
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-08-06 08:09:28 +0530

    Time: 1 ms (99.53%), Space: 63.7 MB (18.35%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,18 +14,56 @@
  * }
  */
 class Solution {
-    public int minDepth(TreeNode root) {
+    public int minDepthDFS(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        
+
         if (root.left == null && root.right == null) {
             return 1;
         }
+
+        int leftDepth = root.left != null ? minDepth(root.left) : Integer.MAX_VALUE;
+        int rightDepth = root.right != null ? minDepth(root.right) : Integer.MAX_VALUE;
         
-        int leftHeight = (root.left != null) ? minDepth(root.left) : Integer.MAX_VALUE;
-        int rightHeight = (root.right != null) ? minDepth(root.right) : Integer.MAX_VALUE;
+        return 1 + Math.min(leftDepth, rightDepth);
+    }
+
+    public int minDepthBFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        int depth = 1;
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size > 0) {
+                TreeNode node = queue.poll();
+
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            depth++;
+        }
         
-        return 1 + Math.min(leftHeight, rightHeight);
+        return depth;
+    }
+
+    public int minDepth(TreeNode root) {
+        return minDepthBFS(root);
     }
 }
