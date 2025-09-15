083b6597255012b5f10870f2c0e802c1066a70cb
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-09-15 08:47:56 +0530

    Time: 0 ms (100%), Space: 46.2 MB (30.83%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,72 +1,41 @@
 class Solution {
-    private int binarySearch(int[] nums, int target, boolean isLeft) {
-        int low = 0;
-        int high = nums.length - 1;
-        int answer = -1;
-        while (low <= high) {
-            int mid = low + (high-low)/2;
-            if (nums[mid] == target) {
-                answer = mid;
-                if (isLeft) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else if (target > nums[mid]) {
-                low = mid + 1;
-            } else if (target < nums[mid]) {
-                high = mid - 1;
-            }
-        }
-        return answer;
-    }
-    private int lower_bound(int[] nums, int target) {
-        int answer = -1;
-        int low = 0, high = nums.length - 1, mid;
-        
+    private int lowerBound(int[] nums, int N, int target) {
+        int low = 0, high = N - 1;
+        int mid;
+        int result = N;
         while (low <= high) {
             mid = low + (high - low)/2;
-            if (nums[mid] == target) {
-                answer = mid;
-            }
-            
-            if (target <= nums[mid]) {
+            if (nums[mid] >= target) {
+                result = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return answer;   
+        return result;
     }
-    
-    private int upper_bound(int[] nums, int target) {
-        int answer = -1;
-        int low = 0, high = nums.length - 1, mid;
-        
+    private int upperBound(int[] nums, int N, int target) {
+        int low = 0, high = N - 1;
+        int mid;
+        int result = N;
         while (low <= high) {
             mid = low + (high - low)/2;
-            if (nums[mid] == target) {
-                answer = mid;
-            }
-            
-            if (target >= nums[mid]) {
-                low = mid + 1;
-            } else {
+            if (nums[mid] > target) {
+                result = mid;
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return answer;
+        return result;
     }
     public int[] searchRange(int[] nums, int target) {
-        int[] answer = new int[2];
-        int firstPosition = lower_bound(nums, target);
-        answer[0] = firstPosition;
-        if (firstPosition == -1) {
-            answer[1] = -1;
-        } else {
-            int secondPosition = upper_bound(nums, target);
-            answer[1] = secondPosition;
+        int N = nums.length;
+        int lb = lowerBound(nums, N, target);
+        if (lb == N || nums[lb] != target) {
+            return new int[]{-1, -1};
         }
-        return answer;
+        int ub = upperBound(nums, N, target);
+        return new int[]{lb, ub - 1};
     }
 }
