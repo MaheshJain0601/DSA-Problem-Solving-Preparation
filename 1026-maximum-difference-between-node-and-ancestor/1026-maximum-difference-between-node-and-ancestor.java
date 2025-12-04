44265c30ba91af11449bdf527034be1e1e36fa1f
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-12-04 10:06:22 +0530

    Time: 0 ms (100%), Space: 43.6 MB (42.05%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -14,21 +14,51 @@
  * }
  */
 class Solution {
-    private int maximumAncestorDifference(TreeNode root, int minValue, int maxValue) {
+    // Brute force
+    // TC: O(N^2)
+    private void findMaxDiffHelper(TreeNode root, TreeNode child, int[] maxDiff) {
+        if (root == null || child == null) return;
+
+        maxDiff[0] = Math.max(maxDiff[0], Math.abs(root.val - child.val));
+
+        findMaxDiffHelper(root, child.left, maxDiff);
+        findMaxDiffHelper(root, child.right, maxDiff);
+    }
+
+    private void findMaxDiff(TreeNode root, int[] maxDiff) {
+        if (root == null) return;
+
+        findMaxDiffHelper(root, root.left, maxDiff);
+        findMaxDiffHelper(root, root.right, maxDiff);
+
+        findMaxDiff(root.left, maxDiff);
+        findMaxDiff(root.right, maxDiff);
+
+    }
+    public int maxAncestorDiffBruteForce(TreeNode root) {
+        int[] maxDiff = new int[1];
+        findMaxDiff(root, maxDiff);
+        return maxDiff[0];
+    }
+
+    // Optimised
+    private int maxAncestorDiffHelper(TreeNode root, int minValue, int maxValue) {
         if (root == null) {
             return Math.abs(maxValue - minValue);
         }
-        
-        maxValue = Math.max(maxValue, root.val);
+
         minValue = Math.min(minValue, root.val);
-        
-        int leftMax = maximumAncestorDifference(root.left, minValue, maxValue);
-        int rightMax = maximumAncestorDifference(root.right, minValue, maxValue);
-        
+        maxValue = Math.max(maxValue, root.val);
+
+        int leftMax = maxAncestorDiffHelper(root.left, minValue, maxValue);
+        int rightMax = maxAncestorDiffHelper(root.right, minValue, maxValue);
+
         return Math.max(leftMax, rightMax);
     }
-    
+
     public int maxAncestorDiff(TreeNode root) {
-        return maximumAncestorDifference(root, root.val, root.val);
+        // int[] maxDiff = new int[1];
+        return maxAncestorDiffHelper(root, root.val, root.val);
+        // return maxDiff[0];
     }
 }
