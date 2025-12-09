d445b7e03ed679a9da8f0ece447050956780bdab
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-12-09 10:30:38 +0530

    Time: 18 ms (15.28%), Space: 48.7 MB (56.31%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,53 @@
+class Solution {
+    static class Triplet {
+        int row;
+        int col;
+        int distance;
+        public Triplet(int row, int col, int distance) {
+            this.row = row;
+            this.col = col;
+            this.distance = distance;
+        }
+    }
+    public int[][] updateMatrix(int[][] mat) {
+        int V = mat.length;
+        int E = mat[0].length;
+
+        int[][] cellDistance = new int[V][E];
+        boolean[][] visited = new boolean[V][E];
+
+        Queue<Triplet> queue = new LinkedList<>();
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < E; ++j) {
+                if (mat[i][j] == 0) {
+                    visited[i][j] = true;
+                    queue.offer(new Triplet(i, j, 0));
+                }
+            }
+        }
+
+        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
+        while (!queue.isEmpty()) {
+            int row = queue.peek().row;
+            int col = queue.peek().col;
+            int distance = queue.peek().distance;
+            queue.poll();
+
+            cellDistance[row][col] = distance;
+
+            for (int[] direction: directions) {
+                int nRow = row + direction[0];
+                int nCol = col + direction[1];
+
+                if (nRow < 0 || nRow >= V || nCol < 0 || nCol >= E || mat[nRow][nCol] == 0 || visited[nRow][nCol]) {
+                    continue;
+                }
+
+                queue.offer(new Triplet(nRow, nCol, distance + 1));
+                visited[nRow][nCol] = true;
+            }
+        }
+        return cellDistance;
+    }
+}
