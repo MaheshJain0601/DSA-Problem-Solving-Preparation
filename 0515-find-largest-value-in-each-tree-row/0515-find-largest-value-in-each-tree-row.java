55b80d255922bb3b9f52093f6839003363d850cf
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-12-17 10:31:12 +0530

    Time: 1 ms (96.12%), Space: 46.7 MB (61.64%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -14,10 +14,10 @@
  * }
  */
 class Solution {
-    public List<Integer> largestValues(TreeNode root) {
+    public List<Integer> largestValuesBFS(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) return result;
-        
+
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
@@ -40,6 +40,27 @@ class Solution {
             }
             result.add(maxElementLevel);
         }
+        return result;
+    }
+
+    // DFS
+    private void dfs(TreeNode root, List<Integer> result, int currDepth) {
+        if (root == null) return;
+
+        if (result.size() == currDepth) 
+            result.add(root.val);
+        else 
+            result.set(currDepth, Math.max(result.get(currDepth), root.val));
+
+        dfs(root.left, result, currDepth + 1);
+        dfs(root.right, result, currDepth + 1);
+    }
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        dfs(root, result, 0);
+
         return result;
     }
 }
