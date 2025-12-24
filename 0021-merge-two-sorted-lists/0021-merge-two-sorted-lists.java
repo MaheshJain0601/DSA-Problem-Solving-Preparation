602b68613689619ad8c0f032a0b781225bdd0694
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-12-24 19:30:09 +0530

    Time: 0 ms (100%), Space: 44.2 MB (45.68%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,55 +10,26 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // T.C: O(N+M)
-        // S.C: O(1)
-        if (list1 == null) {
-            return list2;
-        }
-        if (list2 == null) {
-            return list1;
-        }
-        
-        ListNode ans = new ListNode(0);
-        ListNode tail = ans;
-        
+        ListNode result = new ListNode(-1);
+
+        ListNode curr = result;
         while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                tail.next = list1;
-                tail = list1;
-                list1 = list1.next;
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;  
             } else {
-                tail.next = list2;
-                tail = list2;
-                list2 = list2.next;
+                curr.next = list2;
+                list2 = list2.next;  
             }
+            curr = curr.next;
         }
-        if (list1 == null) {
-            tail.next = list2;
-        } 
-        if (list2 == null) {
-            tail.next = list1;
+
+        if (list1 != null) {
+            curr.next = list1;
         }
-        return ans.next;
+        if (list2 != null) {
+            curr.next = list2;
+        }
+        return result.next;
     }
-    
-    public ListNode recurMergeTwoLists(ListNode list1, ListNode list2) {
-        // T.C: O(N+M)
-        //S.C: O(N+M)
-        if (list1 == null) {
-            return list2;
-        }
-        if (list2 == null) {
-            return list1;
-        }
-        if (list1.val < list2.val) {
-            list1.next = recurMergeTwoLists(list1.next, list2);
-            return list1;
-        }
-        else {
-            list2.next = recurMergeTwoLists(list1, list2.next);
-            return list2;
-        }
-    }
-    
 }
