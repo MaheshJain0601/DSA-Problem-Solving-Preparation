ff1974f9f3116af3ea33a6fc2010eb6aa049dcc9
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-12-31 13:08:33 +0530

    Time: 41 ms (32.19%), Space: 48 MB (8.83%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -19,9 +19,9 @@ class Solution {
     }
 
     // Approach-2 (Three-pass solution)
-    // TC: O(N)
+    // TC: O(3N)
     // SC: O(2N)
-    public int minimumDeletions(String s) {
+    public int minimumDeletionsApproach2(String s) {
         int N = s.length();
         int[] leftCountB = new int[N];
         int[] rightCountA = new int[N];
@@ -45,6 +45,33 @@ class Solution {
         int minDeletion = Integer.MAX_VALUE;
         for (int index = 0; index < N; ++index) {
             minDeletion = Math.min(minDeletion, leftCountB[index] + rightCountA[index]);
+        }
+
+        return minDeletion;
+    }
+
+    // Approach-3 (Two-pass solution)
+    // TC: O(N)
+    // SC: O(2N)
+    public int minimumDeletions(String s) {
+        int N = s.length();
+        int[] rightCountA = new int[N];
+
+        int countA = 0;
+        for (int index = N - 1; index >= 0; --index) {
+            rightCountA[index] = countA;
+            if (s.charAt(index) == 'a') {
+                countA++;
+            }
+        }
+
+        int minDeletion = Integer.MAX_VALUE;
+        int leftCountB = 0;
+        for (int index = 0; index < N; ++index) {
+            minDeletion = Math.min(minDeletion, leftCountB + rightCountA[index]);
+            if (s.charAt(index) == 'b') {
+                leftCountB++;
+            }
         }
 
         return minDeletion;
