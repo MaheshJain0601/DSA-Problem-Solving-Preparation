922022edab4ea41181cd9468507d71c5d4289734
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2025-12-31 15:57:04 +0530

    Time: 2 ms (97.21%), Space: 44.7 MB (86.91%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int longestValidParentheses(String s) {
+    // TC: O(N)
+    // SC: O(N)
+    public int longestValidParenthesesApproach1(String s) {
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
         int maxLen = 0;
@@ -19,5 +21,44 @@ class Solution {
             }
         }
         return maxLen;
+    }
+
+    public int longestValidParentheses(String s) {
+        int maxLength = 0;
+        int openBrackets = 0, closeBracket = 0;
+        // Left to Right pass
+        for (int index = 0; index < s.length(); ++index) {
+            if (s.charAt(index) == '(') {
+                openBrackets++;
+            } else {
+                closeBracket++;
+            }
+
+            if (openBrackets == closeBracket) {
+                maxLength = Math.max(maxLength, openBrackets * 2);
+            } else if (closeBracket > openBrackets) {
+                openBrackets = 0;
+                closeBracket = 0;
+            }
+        }
+
+        openBrackets = closeBracket = 0;
+
+        for (int index = s.length() - 1; index >= 0; --index) {
+            if (s.charAt(index) == '(') {
+                openBrackets++;
+            } else {
+                closeBracket++;
+            }
+
+            if (openBrackets == closeBracket) {
+                maxLength = Math.max(maxLength, openBrackets * 2);
+            } else if (openBrackets > closeBracket) {
+                openBrackets = 0;
+                closeBracket = 0;
+            }
+        }
+
+        return maxLength;
     }
 }
