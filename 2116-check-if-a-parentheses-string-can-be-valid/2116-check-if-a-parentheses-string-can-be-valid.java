8c2d2f265cfcf76814cab6ff5848c5ace05dead4
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-01 18:12:18 +0530

    Time: 24 ms (25.62%), Space: 50.2 MB (5.69%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -1,5 +1,50 @@
 class Solution {
+    // Approach-1: With space
+    // TC: O(N)
+    // SC: O(N)
     public boolean canBeValid(String s, String locked) {
+        int N = s.length();
+
+        if (N % 2 == 1) {
+            return false;
+        }
+
+        int index = 0;
+        char ch, lockedCh;
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> openClosed = new Stack<>();
+
+        while (index < N) {
+            ch = s.charAt(index);
+            lockedCh = locked.charAt(index);
+            if (lockedCh == '0') {
+                openClosed.push(index);
+            } else if (ch == '(') {
+                open.push(index);
+            } else if (ch == ')') {
+                if (!open.isEmpty()) {
+                    open.pop();
+                } else if (!openClosed.isEmpty()) {
+                    openClosed.pop();
+                } else {
+                    return false;
+                }
+            }
+            index++;
+        }
+
+        while (!open.isEmpty() && !openClosed.isEmpty() && open.peek() < openClosed.peek()) {
+            open.pop();
+            openClosed.pop();
+        }
+
+        return open.isEmpty();
+    }
+
+    // Approach-2: Without space
+    // TC: O(2*N)
+    // SC: O(1)
+    public boolean canBeValid1(String s, String locked) {
         int N = s.length();
 
         if (N % 2 == 1) {
