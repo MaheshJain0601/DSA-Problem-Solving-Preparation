dadeecf58450dc4690cfec199be1c3fe94c204cb
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-01 17:23:10 +0530

    Time: 0 ms (100%), Space: 40.6 MB (100%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,22 +1,23 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int openingBrackets = 0, closingBrackets = 0;
-        int N = s.length();
+        
+        int openParenCount = 0;
+		int closeParenCount = 0;
+        int length = s.length();
+        
+		// for (char ch: pattern.toCharArray()) {
+        for (int index = 0; index < length; ++index) {
+            char ch = s.charAt(index);
+			if (ch == '(') {
+				openParenCount++;
+			} else if (ch == ')' && openParenCount > 0) {
+				openParenCount--;
+			} else if (ch == ')') {
+				closeParenCount++;
+			}
+		}
 
-        int index = 0;
-        char ch;
-        while (index < N) {
-            ch = s.charAt(index);
-            if (ch == '(') {
-                openingBrackets++;
-            } else if (openingBrackets > 0) {
-                openingBrackets--;
-            } else {
-                closingBrackets++;
-            }
-            index++;
-        }
-        return openingBrackets + closingBrackets;
+		return openParenCount+closeParenCount;
         
     }
 }
