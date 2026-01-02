81bff79362e848eb6147b009edb6a88a59f7dc4a
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-02 14:38:12 +0530

    Time: 0 ms (100%), Space: 80.4 MB (35.48%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,18 +1,26 @@
 class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        int low = 0, high = arr.length - 1, mid;
-        
+    public int peakIndexInMountainArray(int[] nums) {
+        int N = nums.length;
+        int low = 1, high = N - 2;
+        int mid;
+
         while (low <= high) {
             mid = low + (high - low)/2;
-            
-            if (arr[mid] < arr[mid+1]) {
+            // Peak element
+            if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+                return mid;
+            }
+
+            // Check whether the element exists on left half or right half
+            // Eliminate appropriately
+            if (nums[mid] > nums[mid-1]) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        
-        return low;
+
+        return -1;
         
     }
 }
