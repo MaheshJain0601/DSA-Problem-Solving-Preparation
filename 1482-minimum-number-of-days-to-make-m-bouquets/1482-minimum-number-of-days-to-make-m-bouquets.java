59ad9b240d4762e8926e921645b889d47ace8457
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-03 08:33:54 +0530

    Time: 23 ms (18.81%), Space: 82.9 MB (10.27%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,42 @@
+class Solution {
+    private boolean isPossibleBloomToDay(int[] bloomDay, int day, int m, int k) {
+        int consecutiveDays = 0;
+        int countBouquets = 0;
+        for (int index = 0; index < bloomDay.length; ++index) {
+            if (bloomDay[index] <= day) {
+                consecutiveDays++;
+            } else {
+                countBouquets += (consecutiveDays/k);
+                consecutiveDays = 0;
+            }
+        }
+        countBouquets += (consecutiveDays/k);
+
+        return countBouquets >= m;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int N = bloomDay.length;
+        if (N < m * k) {
+            return -1;
+        }
+
+        int low = Arrays.stream(bloomDay).min().getAsInt();
+        int high = Arrays.stream(bloomDay).max().getAsInt();
+        int maxDay = high;
+
+        int midDay;
+        int result = Integer.MAX_VALUE;
+
+        while (low <= high) {
+            midDay = low + (high-low)/2;
+            if (isPossibleBloomToDay(bloomDay, midDay, m, k)) {
+                result = Math.min(result, midDay);
+                high = midDay - 1;
+            } else {
+                low = midDay + 1;
+            }
+        }
+
+        return result == Integer.MAX_VALUE ? -1 : result; 
+    }
+}
