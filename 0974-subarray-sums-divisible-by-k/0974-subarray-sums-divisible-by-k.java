828fcf2c914460f4ddeb7f1e258f8669a07177e8
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-06 18:04:50 +0530

    Time: 26 ms (26.38%), Space: 51.8 MB (34.63%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,24 +1,29 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int sum = 0;
-        int rem;
-        int result = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        map.put(0, 1);
+        Map<Integer, Integer> remainderCount = new HashMap<>();
+
+        int totalSubarrays = 0;
+        int currSum = 0, rem;
+
+        // For the entire array to be considered
+        remainderCount.put(currSum%k, 1);
+
         for (int index = 0; index < nums.length; ++index) {
-            sum += nums[index];
-            rem = sum % k;
+            currSum += nums[index];
+            rem = currSum % k;
+            // Never store negative remainder, because it loose the previously found element
+            // Let say, = 7 * N - 4(remainder)
+            //          = 7 * N - 4 + 7 - 7
+            //          = 7 (N - 1) + 3
             if (rem < 0) {
                 rem += k;
             }
             
-            if (map.containsKey(rem)) {
-                result += map.get(rem);
+            if (remainderCount.containsKey(rem)) {
+                totalSubarrays += remainderCount.get(rem);
             }
-            map.put(rem, map.getOrDefault(rem, 0) + 1);
+            remainderCount.put(rem, remainderCount.getOrDefault(rem, 0) + 1);
         }
-        
-        return result;
+        return totalSubarrays;
     }
 }
