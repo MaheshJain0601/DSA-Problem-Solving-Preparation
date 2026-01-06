e8157301f56bb4cfb2a5e737e6f76edd7d51c83e
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-06 17:23:06 +0530

    Time: 1 ms (99.74%), Space: 77.3 MB (31.68%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,8 +3,10 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        int currSum = nums[0], maxSum = nums[0];
         
+        int maxSum = nums[0];
+        int currSum = nums[0];
+
         for (int index = 1; index < nums.length; ++index) {
             currSum = Math.max(nums[index], currSum + nums[index]);
             maxSum = Math.max(maxSum, currSum);
