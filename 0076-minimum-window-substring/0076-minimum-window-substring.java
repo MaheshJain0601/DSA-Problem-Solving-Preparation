ad9c5603d7b3bb7a29ed3af3f905010d8dd94763
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-12 14:05:02 +0530

    Time: 13 ms (66.49%), Space: 46.8 MB (62.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,41 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
         String DEFAULT_ANSWER = "";
-        if (t.length() > s.length()) {
-            return DEFAULT_ANSWER;
-        }
+        int tLength = t.length();
+        int sLength = s.length();
+        if (tLength > sLength) return DEFAULT_ANSWER;
 
-        Map<Character, Integer> freq = new HashMap<>();
-
+        int minWindowSize = Integer.MAX_VALUE;
+        int minStrStartIndex = Integer.MAX_VALUE;
+        int requiredCount = tLength;
+        
+        Map<Character, Integer> freqMap = new HashMap<>();
         for (char ch: t.toCharArray()) {
-            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
         }
-
         int i = 0, j = 0;
-        int minWindow = Integer.MAX_VALUE;
-        int windowStart = -1;
-        int requiredCount = t.length();
-        while (j < s.length()) {
-            char ch = s.charAt(j);
-            if (freq.containsKey(ch) && freq.get(ch) > 0) {
-                requiredCount -= 1;
+        int currFreq;
+        char ch;
+
+        while (j < sLength) {
+            ch = s.charAt(j);
+            currFreq = freqMap.getOrDefault(ch, 0);
+            if (currFreq > 0) {
+                requiredCount -= 1;   
             }
-            freq.put(ch, freq.getOrDefault(ch, 0) - 1);
+            freqMap.put(ch, currFreq - 1);
 
             while (i <= j && requiredCount == 0) {
-                if (j - i + 1 < minWindow) {
-                    minWindow = j - i + 1;
-                    windowStart = i;
+                if (minWindowSize >= j - i + 1) {
+                    minWindowSize = j - i + 1;
+                    minStrStartIndex = i;
                 }
-                freq.put(s.charAt(i), freq.getOrDefault(s.charAt(i), 0) + 1);
-                if (freq.get(s.charAt(i)) > 0) {
+                ch = s.charAt(i);
+                freqMap.put(ch, freqMap.get(ch) + 1);
+                if (freqMap.get(ch) > 0) {
                     requiredCount += 1;
                 }
                 i++;
             }
             j++;
         }
-
-        return minWindow == Integer.MAX_VALUE ? DEFAULT_ANSWER : s.substring(windowStart, windowStart + minWindow);
+        return minWindowSize == Integer.MAX_VALUE ? DEFAULT_ANSWER : s.substring(minStrStartIndex, minStrStartIndex + minWindowSize);
+        
     }
 }
