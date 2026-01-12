cbc101545725cbe1d25de8826223855a33d81d3a
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-01-12 15:14:39 +0530

    Time: 24 ms (56.96%), Space: 75.7 MB (90.16%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,40 +1,40 @@
 class Solution {
+    // Approach - 1
+    // TC: O(N)
+    // SC: O(N)
+    public boolean containsNearbyDuplicateApproach1(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int index = 0; index < nums.length; ++index) {
+            if (map.containsKey(nums[index])) {
+                Integer val = map.get(nums[index]);
+                if (Math.abs(val.intValue() - index) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[index], index);
+        }
+        return false;
+    }
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Approach - 1
-//         Map<Integer, Integer> map = new HashMap<>();
-        
-//         for (int i = 0; i < nums.length; ++i) {
-//             if (map.containsKey(nums[i])) {
-//                 Integer value = map.get(nums[i]);
-//                 if (Math.abs(value.intValue() - i) <= k) {
-//                     return true;
-//                 }
-//             }
-//             map.put(nums[i], i);
-//         }
-        
-//         return false;
-        
-        // if i and j are both of same indices
         if (k == 0) {
             return false;
         }
-        
-        Set<Integer> slidingWindowHash = new HashSet<>();
-        
-        for (int index = 0; index < nums.length; ++index) {
-            
-            if (slidingWindowHash.contains(nums[index])) {
+
+        int index = 0;
+        Set<Integer> hash = new HashSet<>();
+
+        while (index < nums.length) {
+            if (hash.contains(nums[index])) {
                 return true;
             }
             
             if (index >= k) {
-                slidingWindowHash.remove(nums[index-k]);
+                hash.remove(nums[index-k]);
             }
-            
-            slidingWindowHash.add(nums[index]);
+            hash.add(nums[index]);
+            index++;
         }
-        
         return false;
     }
 }
