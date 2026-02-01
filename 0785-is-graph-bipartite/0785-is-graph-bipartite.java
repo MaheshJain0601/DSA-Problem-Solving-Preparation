f5ea99050d50e43722e476493e4accf6eeb4cd53
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-01 08:56:43 +0530

    Time: 1 ms (61.44%), Space: 47.2 MB (50.87%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,10 +1,8 @@
 class Solution {
-    // BFS
     private boolean bfs(int[][] graph, int[] colors, int src) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(src);
         colors[src] = 0;
-
         while (!queue.isEmpty()) {
             int node = queue.poll();
 
@@ -12,14 +10,14 @@ class Solution {
                 if (colors[adjNode] == -1) {
                     colors[adjNode] = 1 - colors[node];
                     queue.offer(adjNode);
-                } else if (colors[node] == colors[adjNode]) {
+                } else if (colors[adjNode] == colors[node]) {
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean isBipartiteBFS(int[][] graph) {
+    public boolean isBipartite(int[][] graph) {
         int V = graph.length;
         int E = graph[0].length;
 
@@ -31,39 +29,6 @@ class Solution {
                 if (!bfs(graph, colors, i)) {
                     return false;
                 }
-            }
-        }
-        return true;
-    }
-
-    // DFS
-    public boolean isBipartite(int[][] graph) {
-        int V = graph.length;
-        int E = graph[0].length;
-
-        int[] colors = new int[V];
-        Arrays.fill(colors, -1);
-        
-        for (int i = 0; i < V; ++i) {
-            if (colors[i] == -1) {
-                if (!dfs(graph, colors, i, 0)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private boolean dfs(int[][] graph, int[] colors, int src, int color) {
-        colors[src] = color;
-
-        for (int adjNode: graph[src]) {
-            if (colors[adjNode] == -1) {
-                if(!dfs(graph, colors, adjNode, 1 - color)) {
-                    return false;
-                }
-            } else if (colors[adjNode] == color) {
-                return false;
             }
         }
         return true;
