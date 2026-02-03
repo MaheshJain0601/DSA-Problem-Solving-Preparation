0c0c2503c99d2774d3f201ffa0297e83ec44730d
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-03 08:10:45 +0530

    Time: 1 ms (72.33%), Space: 43.2 MB (97.48%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,28 +14,31 @@
  * }
  */
 class Solution {
-    private void rightSideViewHelper(TreeNode root, List<Integer> result, int level) {
-        if (root == null) {
-            return;
-        }
-
-        if (level == result.size()) {
-            result.add(root.val);
-        }
-
-        rightSideViewHelper(root.right, result, level + 1);
-        rightSideViewHelper(root.left, result, level + 1);
-
-    }
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
         }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
 
-        rightSideViewHelper(root, result, 0);
+        int levelSize;
+        while (!queue.isEmpty()) {
+            levelSize = queue.size();
+            TreeNode node = null;
+            while (levelSize > 0) {
+                node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                levelSize--;
+            }
+            result.add(node.val);
+        }
 
-
-        return result; 
+        return result;
     }
 }
