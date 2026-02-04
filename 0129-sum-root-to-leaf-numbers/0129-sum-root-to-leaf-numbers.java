6ff88b775e759be8959b40869134544809175d74
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-04 09:59:11 +0530

    Time: 0 ms (100%), Space: 42.8 MB (80.45%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,29 +14,29 @@
  * }
  */
 class Solution {
-    private boolean isLeafNode(TreeNode node) {
-        return node.left == null && node.right == null;
+    private boolean isLeafNode(TreeNode root) {
+        return root.left == null && root.right == null;
     }
     private void sumNumberHelper(TreeNode root, int[] sumRootToLeaf, int currSum) {
         if (root == null) {
             return;
         }
-
-        currSum = currSum*10 + root.val;
-
+        
+        currSum = (currSum * 10) + root.val;
         if (isLeafNode(root)) {
             sumRootToLeaf[0] += currSum;
             return;
         }
-
+        
         sumNumberHelper(root.left, sumRootToLeaf, currSum);
         sumNumberHelper(root.right, sumRootToLeaf, currSum);
+        return;
     }
 
     public int sumNumbers(TreeNode root) {
         int[] sumRootToLeaf = new int[1];
         int currSum = 0;
-        sumNumberHelper(root, sumRootToLeaf, 0);
+        sumNumberHelper(root, sumRootToLeaf, currSum);
         return sumRootToLeaf[0];
     }
 }
