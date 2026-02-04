e7c430eff0e951da1be818e00170eb1a8e071a49
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-04 07:38:13 +0530

    Time: 0 ms (100%), Space: 42.5 MB (99.91%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,17 +14,21 @@
  * }
  */
 class Solution {
-    private boolean isSymmetricHelper(TreeNode root1, TreeNode root2) {
+    private boolean isLeafNode(TreeNode root) {
+        return root.left == null && root.right == null;
+    }
+    
+    public boolean isSymmetricHelper(TreeNode root1, TreeNode root2) {
         if (root1 == null || root2 == null) {
             return root1 == root2;
         }
-
-        return root1.val == root2.val &&
-            isSymmetricHelper(root1.left, root2.right) &&
-            isSymmetricHelper(root1.right, root2.left);
+        return (root1.val == root2.val)
+            && isSymmetricHelper(root1.left, root2.right)
+            && isSymmetricHelper(root1.right, root2.left);
     }
+    
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
+        if (root == null || isLeafNode(root)) {
             return true;
         }
         return isSymmetricHelper(root.left, root.right);
