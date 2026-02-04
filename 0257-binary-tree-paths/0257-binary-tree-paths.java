0bb11b36efb99168087f5940eafb8df1af4bb7ff
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-04 16:08:17 +0530

    Time: 2 ms (80.93%), Space: 43.7 MB (98.22%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -14,34 +14,29 @@
  * }
  */
 class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new LinkedList<>();
-        if (root == null) {
-            return result;
-        }
-        
-        String path = Integer.toString(root.val);
-        if (root.left == null && root.right == null) {
-            result.add(path);
-        }
-        dfs(root.left, result, path);
-        dfs(root.right, result, path);
-        
-        
-        return result;
+    private boolean isLeafNode(TreeNode root) {
+        return root.left == null && root.right == null;
     }
-    
-    public void dfs(TreeNode node, List<String> result, String path) {
-        if (node == null) {
+    private void binaryTreePathsHelper(TreeNode root, List<String> path, List<String> result) {
+        if (root == null) {
             return;
         }
-        
-        path = path + "->" + Integer.toString(node.val);
-        if (node.left == null && node.right == null) {
-            result.add(path);
+
+        path.add(Integer.toString(root.val));
+        if (isLeafNode(root)) {
+            result.add(String.join("->", path));
         }
         
-        dfs(node.left, result, path);
-        dfs(node.right, result, path);
+        binaryTreePathsHelper(root.left, path, result);
+        binaryTreePathsHelper(root.right, path, result);
+
+        path.remove(path.size() - 1);        
+    }
+    
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> path = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        binaryTreePathsHelper(root, path, result);
+        return result;
     }
 }
