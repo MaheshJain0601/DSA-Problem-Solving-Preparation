bfb5fc3e986175d409cc4c076e3f08e82416a621
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-04 07:34:45 +0530

    Time: 6 ms (97.54%), Space: 47 MB (96.9%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    // TC: O(N^2)
     private String findDuplicateSubtreesHelper(TreeNode root, Map<String, Integer> hashMap, List<TreeNode> result) {
         if (root == null) {
             return "N";
@@ -30,11 +31,49 @@ class Solution {
 
         return currentStructure;
     }
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+    public List<TreeNode> findDuplicateSubtreesSimple(TreeNode root) {
         Map<String, Integer> hashMap = new HashMap<>();
 
         List<TreeNode> result = new ArrayList<>();
         findDuplicateSubtreesHelper(root, hashMap, result);
+
+        return result;
+    }
+    // Approach-2
+    private int nextSubtreeId = 1; // 0 reserved for null subtree
+
+    private int findDuplicateSubtreesHelperV2(TreeNode root, List<TreeNode> result, 
+                    Map<String, Integer> signatureToId,
+                    Map<Integer, Integer> freq) {
+        if (root == null) return 0;
+
+        int leftId = findDuplicateSubtreesHelperV2(root.left, result, signatureToId, freq);
+        int rightId = findDuplicateSubtreesHelperV2(root.right, result, signatureToId, freq);
+
+        String signature = new StringBuilder()
+                .append(root.val).append(',')
+                .append(leftId).append(',')
+                .append(rightId)
+                .toString();
+
+        int id = signatureToId.computeIfAbsent(signature, s -> nextSubtreeId++);
+        // System.out.println(signatureToId);
+
+        int newCount = freq.getOrDefault(id, 0) + 1;
+        freq.put(id, newCount);
+
+        if (newCount == 2) result.add(root);
+
+        return id;
+    }
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        List<TreeNode> result = new ArrayList<>();
+        Map<String, Integer> signatureToId = new HashMap<>();   // String is correct key
+        Map<Integer, Integer> freq = new HashMap<>();
+        findDuplicateSubtreesHelperV2(root, result, signatureToId, freq);
 
         return result;
     }
