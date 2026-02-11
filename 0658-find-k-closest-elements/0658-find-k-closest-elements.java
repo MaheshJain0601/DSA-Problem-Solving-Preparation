e54aeafd3cd8a12b8b5476f1eb20a84b72f72fde
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-11 17:18:16 +0530

    Time: 4 ms (98.15%), Space: 48.2 MB (21.11%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -8,7 +8,9 @@ class Solution {
         }
     }
     // Brute force
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    // TC: O(nlogk) + O(klogk)
+    // SC: O(k) + O(k)
+    public List<Integer> findClosestElementsBrute(int[] arr, int k, int x) {
         Queue<Pair> pq = new PriorityQueue<>((a, b) -> {
             if (b.first != a.first) {
                 return Integer.compare(b.first, a.first);
@@ -26,6 +28,53 @@ class Solution {
             result.add(pq.poll().second);
         }
         Collections.sort(result);
+        return result;
+    }
+
+    // Better Solution
+    // TC: O(N)
+    // SC: O(N)
+    public List<Integer> findClosestElementsBetter(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<Integer>();
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (high - low >= k) {
+            int distLow = Math.abs(arr[low] - x); 
+            int distHigh = Math.abs(arr[high] - x);
+
+            if (distLow <= distHigh) {
+                high--;         
+            } else {
+                low ++;
+            }
+        }
+
+        while(low <= high)
+            result.add(arr[low++]);
+
+        return result;
+    }
+
+    // Optimal
+    // TC: O(log (N-K) + K)
+    // SC: O(N)
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - k;
+        while (left < right){
+            int mid = left + (right - left)/2;
+            if (x - arr[mid] > arr[mid+k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        List<Integer> result = new ArrayList();
+        for(int i = left ; i < left+k ; i++){
+            result.add(arr[i]);
+        }
         return result;
     }
 }
