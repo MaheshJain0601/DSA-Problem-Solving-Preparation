f9782424d6ae4ecc6c4254dee2c2374a6f20b179
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-12 16:47:28 +0530

    Time: 13 ms (64.33%), Space: 47.2 MB (53.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,45 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
         String DEFAULT_ANSWER = "";
-        int tLength = t.length();
         int sLength = s.length();
+        int tLength = t.length();
         if (tLength > sLength) return DEFAULT_ANSWER;
 
-        int minWindowSize = Integer.MAX_VALUE;
-        int minStrStartIndex = Integer.MAX_VALUE;
-        int requiredCount = tLength;
-        
         Map<Character, Integer> freqMap = new HashMap<>();
-        for (char ch: t.toCharArray()) {
+        char ch;
+        for (int index = 0; index < tLength; ++index) {
+            ch = t.charAt(index);
             freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
         }
+
+        int requiredCount = tLength;
+        int minWindowSize = Integer.MAX_VALUE;
+        int minStrStartIndex = Integer.MAX_VALUE;
         int i = 0, j = 0;
         int currFreq;
-        char ch;
 
         while (j < sLength) {
             ch = s.charAt(j);
             currFreq = freqMap.getOrDefault(ch, 0);
             if (currFreq > 0) {
-                requiredCount -= 1;   
+                requiredCount--;
             }
             freqMap.put(ch, currFreq - 1);
 
+
             while (i <= j && requiredCount == 0) {
-                if (minWindowSize >= j - i + 1) {
+                if (minWindowSize > j - i + 1) {
                     minWindowSize = j - i + 1;
                     minStrStartIndex = i;
                 }
                 ch = s.charAt(i);
                 freqMap.put(ch, freqMap.get(ch) + 1);
                 if (freqMap.get(ch) > 0) {
-                    requiredCount += 1;
+                    requiredCount++;
                 }
                 i++;
             }
             j++;
         }
         return minWindowSize == Integer.MAX_VALUE ? DEFAULT_ANSWER : s.substring(minStrStartIndex, minStrStartIndex + minWindowSize);
-        
     }
 }
