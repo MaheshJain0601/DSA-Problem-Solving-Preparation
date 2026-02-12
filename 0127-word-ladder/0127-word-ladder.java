727798af24d2508847ddb291dc17c2d10d21ad51
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-12 11:58:16 +0530

    Time: 76 ms (61.09%), Space: 48.7 MB (43.13%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,40 @@
+class Solution {
+    class Pair{
+        String first;
+        int second;
+        Pair(String first,int second){
+            this.first=first;
+            this.second=second;
+        }
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+        Set<String> lookup = new HashSet<>(wordList);
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(beginWord, 1));
+        lookup.remove(beginWord);
+
+        while (!queue.isEmpty()) {
+            String currWord = queue.peek().first;
+            int moves = queue.peek().second;
+            queue.poll();
+            if (endWord.equals(currWord)) {
+                return moves;
+            }
+
+            for (int index = 0; index < currWord.length(); ++index) {
+                char[] replacementCharArray = currWord.toCharArray();
+                for (char ch = 'a'; ch <= 'z'; ++ch) {
+                    replacementCharArray[index] = ch;
+                    String newOption = new String(replacementCharArray);
+                    if (lookup.contains(newOption)) {
+                        queue.offer(new Pair(newOption, moves + 1));
+                        lookup.remove(newOption);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
