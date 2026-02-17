b1b45ef90da329c39675e544ba5b6f97a69ed14c
Vinod Mahesh Jain <55226441+MaheshJain0601@users.noreply.github.com>
2026-02-17 15:22:22 +0530

    Time: 683 ms (66.04%), Space: 62.7 MB (81.13%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,5 +1,29 @@
 class Solution {
-    private long countProducts(int[] nums1, int[] nums2, long targetProduct) {
+    private long findMinProduct(int[] A, int[] B) {
+        long minA = A[0], maxA = A[A.length - 1];
+        long minB = B[0], maxB = B[B.length - 1];
+
+        long p1 = minA * minB;
+        long p2 = minA * maxB;
+        long p3 = maxA * minB;
+        long p4 = maxA * maxB;
+
+        return Math.min(Math.min(p1, p2), Math.min(p3, p4));
+    }
+
+    private long findMaxProduct(int[] A, int[] B) {
+        long minA = A[0], maxA = A[A.length - 1];
+        long minB = B[0], maxB = B[B.length - 1];
+
+        long p1 = minA * minB;
+        long p2 = minA * maxB;
+        long p3 = maxA * minB;
+        long p4 = maxA * maxB;
+
+        return Math.max(Math.max(p1, p2), Math.max(p3, p4));
+    }
+
+    private long countLessOrEqual(int[] nums1, int[] nums2, long targetProduct) {
         long count = 0;
         for (int num1 : nums1) {
             if (num1 == 0) {
@@ -26,15 +50,15 @@ class Solution {
     }
 
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        long left = (long) -1e10; // Minimum product possible
-        long right = (long) 1e10; // Maximum product possible
+        long left = findMinProduct(nums1, nums2);
+        long right = findMaxProduct(nums1, nums2);
 
         long result = 0;
         
         while (left <= right) {
             long midProduct = left + (right - left) / 2;
 
-            long countSmallest = countProducts(nums1, nums2, midProduct);
+            long countSmallest = countLessOrEqual(nums1, nums2, midProduct);
             if (countSmallest >= k) {
                 result = midProduct; 
                 right = midProduct - 1;
